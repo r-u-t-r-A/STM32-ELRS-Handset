89f2004ce0de55550bb6fb556636546d4521b063
r-u-t-r-A <98609430+r-u-t-r-A@users.noreply.github.com>
2022-03-17 20:43:28 +0100

    Delete include directory

--- include/EEPROM_FUNCTIONS.h
+++
@@ -1,7 +0,0 @@
-
-
-
-void EEPROM_write(unsigned int addr, byte eeprom_data)  {}
-//byte EEPROM_read(unsigned int addr)  {}
-void EEPROM_update(unsigned int addr, byte to_write) {}
-void reset_eeprom() {}
